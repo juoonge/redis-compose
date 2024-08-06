2671377f4b18733df3e166cf7e92b14f63150f89
juoonge <juoonge7646@naver.com>
2024-08-06 11:48:23 +0900

    connect redis

---
+++ src/main/java/com/sparta/rediscompose/RedisComposeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.rediscompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisComposeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisComposeApplication.class, args);
+    }
+
+}
